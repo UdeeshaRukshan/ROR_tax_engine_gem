754b8a430875c9d2717f62308b97133bcbd7ff8d
udeesharukshan <it21807480@my.sliit.lk>
2026-03-11 12:27:29 +0530

    Feat: Initialize the gem structure

---
+++ lib/my_public_gem.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "my_public_gem/version"
+
+module MyPublicGem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/my_public_gem/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MyPublicGem
+  VERSION = "0.1.0"
+end
